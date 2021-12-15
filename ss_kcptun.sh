624bf023b029dc3897784b6f1ac0fdcc1dca8004
Dear.free <62785542+future-death@users.noreply.github.com>
2021-12-15 20:42:51 +0800

    Update ss_kcptun.sh

--- ss_kcptun.sh
+++ ss_kcptun.sh
@@ -2,11 +2,11 @@
 vi rule.v4
 vi kcpconfig.json
 vi ssconfig.json
-iptables-restore < /root/rule.v4
+iptables-restore < /root/ss_and_kcptun/rule.v4
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
 python get-pip.py
 pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
 wget https://github.com/xtaci/kcptun/releases/download/v20210922/kcptun-linux-amd64-20210922.tar.gz
 tar -zxvf kcptun-linux-amd64-20210922.tar.gz
-ssserver -c /root/ssconfig.json -d start
-nohup ./server_linux_amd64 -c "/root/kcpconfig.json" &
+ssserver -c /root/ss_and_kcptun/ssconfig.json -d start
+nohup ./server_linux_amd64 -c "/root/ss_and_kcptun/kcpconfig.json" &
