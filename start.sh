e0cf7aadef1c12c71d00881f51f8cb9994a5a400
Dear.free <62785542+future-death@users.noreply.github.com>
2021-12-15 22:30:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-iptables-restore < /root/ss_and_kcptun/rule.v4
 ssserver -c /root/ss_and_kcptun/ssconfig.json -d start
 nohup ./server_linux_amd64 -c "/root/ss_and_kcptun/kcpconfig.json" &
