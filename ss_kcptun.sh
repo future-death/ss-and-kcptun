c6f8bb1d629064a37d610bb70ef93d071897ce5c
Dear.free <62785542+future-death@users.noreply.github.com>
2021-12-17 12:36:12 +0800

    Update ss_kcptun.sh

--- ss_kcptun.sh
+++ ss_kcptun.sh
@@ -4,5 +4,5 @@ python get-pip.py
 pip install https://github.com/shadowsocks/shadowsocks/archive/master.zip -U
 wget https://github.com/xtaci/kcptun/releases/download/v20210922/kcptun-linux-amd64-20210922.tar.gz
 tar -zxvf kcptun-linux-amd64-20210922.tar.gz
-ssserver -c /root/ss_and_kcptun/ssconfig.json -d start
-nohup ./server_linux_amd64 -c "/root/ss_and_kcptun/kcpconfig.json" &
+ssserver -c /root/ss-and-kcptun/ssconfig.json -d start
+nohup ./server_linux_amd64 -c "/root/ss-and-kcptun/kcpconfig.json" &
