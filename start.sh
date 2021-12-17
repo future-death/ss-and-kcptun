d594fa4667bb27a52908d59895d2b54ff389521e
Dear.free <62785542+future-death@users.noreply.github.com>
2021-12-17 12:35:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-ssserver -c /root/ss_and_kcptun/ssconfig.json -d start
-nohup ./server_linux_amd64 -c "/root/ss_and_kcptun/kcpconfig.json" &
+ssserver -c /root/ss-and-kcptun/ssconfig.json -d start
+nohup ./server_linux_amd64 -c "/root/ss-and-kcptun/kcpconfig.json" &
